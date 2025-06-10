4e2fce9302d3491c4d5288477bb2224a177fe8fc
RazFernandez <mr.fernandez.mx@outlook.com>
2025-06-10 16:20:26 -0500

    Added color theme and fonts to the project

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ function App() {
   return (
     <>
       <div>
-        <h1 className='text-primary'>Hello World!!!</h1>
-        <h2 className='text-primary'>Hello World!!!</h2>
+        <h1 className='text-primary-50 font-exo2'>Hello World!!!</h1>
+        <h2 className='text-primary-500 font-lato'>Hello World!!!</h2>
       </div>
     </>
   )
