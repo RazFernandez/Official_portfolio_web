d662e660b6be965238873ae2594e288796b40a74
RazFernandez <mr.fernandez.mx@outlook.com>
2025-06-10 16:16:37 -0500

    Added tailwind correctly to the project

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,10 @@ function App() {
 
   return (
     <>
-      
+      <div>
+        <h1 className='text-primary'>Hello World!!!</h1>
+        <h2 className='text-primary'>Hello World!!!</h2>
+      </div>
     </>
   )
 }
