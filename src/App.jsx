375a728c1e2ac88bd29fabb0f740aba4e63bab4f
RazFernandez <mr.fernandez.mx@outlook.com>
2025-06-12 16:07:58 -0500

    Added default background color
    Also added navbar image

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,13 @@ function App() {
 
   return (
     <>
-      <body className='bg-dark-bg-600'>
+      
         <Navbar></Navbar>
         <div>
         <h1 className='text-primary-50 font-exo2'>Hello World!!!</h1>
         <h2 className='text-primary-500 font-lato'>Hello World!!!</h2>
         </div>
-      </body>
+
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,12 @@
+import myLogo from '../assets/images/VentoTech-icon.png';
+
 export function Navbar() {
     return (
         <nav className="bg-dark-bg-900">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <a></a>
+                <a className="flex items-center space-x-3 rtl:space-x-reverse pb-0 md:pb-4 xl:pb-0">
+                    <img src={myLogo} className="h-8" alt="Portfolio Logo"></img>
+                </a>
             </div>
 
             {/* <h1>Miguel Fernandez</h1>
