c30a0d23c08ea0aec96c537d30ada9d0562c3cb5
RazFernandez <mr.fernandez.mx@outlook.com>
2025-06-12 15:06:24 -0500

    Initial Navbar building

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-
 import './App.css'
-
+import { Navbar } from './components/Navbar'
 function App() {
   
 
   return (
     <>
-      <div>
+      <body className='bg-dark-bg-600'>
+        <Navbar></Navbar>
+        <div>
         <h1 className='text-primary-50 font-exo2'>Hello World!!!</h1>
         <h2 className='text-primary-500 font-lato'>Hello World!!!</h2>
-      </div>
+        </div>
+      </body>
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+export function Navbar() {
+    return (
+        <nav className="bg-dark-bg-900">
+            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <a></a>
+            </div>
+
+            {/* <h1>Miguel Fernandez</h1>
+            <ul>
+                <li>Home</li>
+                <li>About Me</li>
+                <li>Portfolio</li>
+            </ul> */}
+        </nav>
+    )
+}
