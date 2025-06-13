2d7a0c270172a5d3fa80b21e61e860421b056451
RazFernandez <mr.fernandez.mx@outlook.com>
2025-06-12 20:47:50 -0500

    Added icons svg for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,16 +5,10 @@ export function Navbar() {
         <nav className="bg-dark-bg-900">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a className="flex items-center space-x-3 rtl:space-x-reverse pb-0 md:pb-4 xl:pb-0">
-                    <img src={myLogo} className="h-8" alt="Portfolio Logo"></img>
+                    <img src={myLogo} className="h-8" alt="Portfolio Logo"/>
+                    <span class="self-center text-2xl font-exo2 whitespace-nowrap text-white">MIGUEL FERNANDEZ</span>
                 </a>
             </div>
-
-            {/* <h1>Miguel Fernandez</h1>
-            <ul>
-                <li>Home</li>
-                <li>About Me</li>
-                <li>Portfolio</li>
-            </ul> */}
         </nav>
     )
 }
