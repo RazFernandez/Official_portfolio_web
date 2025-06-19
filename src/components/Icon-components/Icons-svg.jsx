8b9c7c0feb5747beb439e7582c61471a557ca6ba
RazFernandez <mr.fernandez.mx@outlook.com>
2025-06-19 16:21:17 -0500

    Bug fixed, DropMenuIcon now return the svg

--- src/components/Icon-components/Icons-svg.jsx
+++ src/components/Icon-components/Icons-svg.jsx
@@ -100,20 +100,23 @@ export function MediumIcon({ onHover }) {
 
 
 export function DropMenuIcon() { 
-    <svg
-        className="w-2.5 h-2.5 ms-2.5"
-        aria-hidden="true"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 10 6"
-    >
-        <path
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="m1 1 4 4 4-4"
-        />
-    </svg>
+
+    return (
+        <svg
+            className="w-2.5 h-2.5 ms-2.5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 10 6"
+        >
+            <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m1 1 4 4 4-4"
+            />
+        </svg>
+    );
 }
 
