9a590873580f21efc37a7236ec52227ff64bad52
RazFernandez <mr.fernandez.mx@outlook.com>
2025-06-23 16:36:50 -0500

    icons folder from assets is not longer inside images folder

--- src/Pages/Home/hero-logo.jsx
+++
@@ -1,9 +0,0 @@
-import Logo from '../../assets/images/icons/Logo-PortfolioWeb.svg'
-
-export default function HeroLogoPage() {
-    return (
-        <div>
-            <img src={Logo} width={250} ></img>
-        </div>
-    );
-}
