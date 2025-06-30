7f93c1c44494d6eca46123763c62b9a71ebc6a85
RazFernandez <mr.fernandez.mx@outlook.com>
2025-06-30 13:45:01 -0500

    grid layout fixed and now responsive

--- src/components/layout/grid3x3Layout.jsx
+++ src/components/layout/grid3x3Layout.jsx
@@ -1,6 +1,6 @@
 export default function Grid3x3Layout({children}) {
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 p-4">
+        <div className="mx-auto max-w-screen-xl grid grid-cols-1 sm:grid-cols-3 gap-4 p-4">
             {children}
         </div>
     );
