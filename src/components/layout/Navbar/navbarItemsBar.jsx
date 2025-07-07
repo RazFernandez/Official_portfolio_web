d68250b979a2cf19fd654812f8e9c1272f81e9a1
RazFernandez <mr.fernandez.mx@outlook.com>
2025-07-07 18:40:55 -0500

    Routing added to Navbar Elements
    Refactoring components is needed to enhace user experience.

--- src/components/layout/Navbar/navbarItemsBar.jsx
+++ src/components/layout/Navbar/navbarItemsBar.jsx
@@ -14,10 +14,10 @@ export default function NavbarItemBar({ isOpen }) {
     return (
         <div className={`w-full ${isOpen ? "block" : "hidden"} md:block md:w-auto`}>
             <ul className="font-exo2 flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 bg-dark-bg-700 md:bg-dark-bg-900 dark:border-gray-700">
-                <NavbarItem isCurrentPage={true}>HOME</NavbarItem>
-                <NavbarItem>ABOUT ME</NavbarItem>
-                <NavbarItem>PORTFOLIO</NavbarItem>
-                <NavbarItem>SKILLS</NavbarItem>
+                <NavbarItem isCurrentPage={true} path={"/"}>HOME</NavbarItem>
+                <NavbarItem path={"/about"}>ABOUT ME</NavbarItem>
+                <NavbarItem path={"/portfolio"}>PORTFOLIO</NavbarItem>
+                <NavbarItem path={"/skills"}>SKILLS</NavbarItem>
                 <NavBarItemDropdown dropdownName={"SOCIAL MEDIA"} onClickEvent={() => setIsMenuOpen(!isMenuOpen)}>
                     <NavbarMenuItems isOpen={isMenuOpen}>
                         <NavbarSubItem>LinkedIn</NavbarSubItem>
