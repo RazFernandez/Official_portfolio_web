92842e71d7d595e1625a41d1641769a8be72d9e5
RazFernandez <mr.fernandez.mx@outlook.com>
2025-07-15 17:22:33 -0500

    Fixed responsivess in groupLayouts (Projectcard and tech tool box)

--- src/components/layout/grid3x3Layout.jsx
+++ src/components/layout/grid3x3Layout.jsx
@@ -1,15 +1,18 @@
-export default function Grid3x3Layout({children}) {
+export default function Grid3x3Layout({ children }) {
     return (
-        <div className="mx-auto max-w-screen-xl grid grid-cols-1 sm:grid-cols-3 gap-4 p-4">
-            {children}
+        <div className="flex items-center justify-center">
+            <div className="mx-auto max-w-screen-xl grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+                {children}
+            </div>
         </div>
     );
 }
 
+
 export function Grid4x3Layout({ children }) {
     return (
         <div className="w-full flex justify-center">
-            <div className="grid gap-8 grid-cols-2 sm:grid-cols-5 md:gap-16 p-4">
+            <div className="grid gap-8 grid-cols-2 md:grid-cols-4 lg:grid-cols-5 lg:gap-16 p-4">
                 {children}
             </div>
         </div>
